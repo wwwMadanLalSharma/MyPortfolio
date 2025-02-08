d1c097a81f62e24905372974613aa250952547ac
wwwMadanLalSharma <mad.sharma94@gmail.com>
2025-02-07 21:13:41 -0500

    Initial commit of MyPortfolio website

--- obj/Release/net8.0/MyPortfolio.AssemblyInfo.cs
+++ obj/Release/net8.0/MyPortfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+532480b7309573bb2979172cb21164b619f9288f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
